2adf30dd31654efb187b92e750617aa9827bdd2e
Maryam <maryam.oxford@gmail.com>
2017-12-21 20:24:11 +0000

    This is so gross

--- hansard.py
+++ hansard.py
@@ -1,10 +1,12 @@
 # import libraries
 import urllib2
+import requests
 from bs4 import BeautifulSoup
 import math
 from datetime import datetime
 import matplotlib
 import matplotlib.pyplot as plt
+import re
 
 def getOldContribs(searchTerm):
 
@@ -38,7 +40,71 @@ def getOldContribs(searchTerm):
 
     return contributionDates
 
-def getNewContribs(searchTerm,contributionDates):
+def getMidContribs(searchTerm): # this is super gross
+
+    contributionDates = []
+
+    # search archive from 2005-2010
+
+    days = ['01', '02', '03', '04', '05', '06', '07' ,'08' ,'09', '10', '11' ,'12', '13', '14' ,'15',
+            '16', '17' ,'18' ,'19' ,'20' ,'21' ,'22' ,'23', '24' ,'25' ,'26' ,'27', '28' ,'29', '30' ,'31']
+    months = ['01', '02' ,'03', '04', '05' ,'06', '07', '08', '09', '10', '11', '12']
+    years = ['2005', '2006', '2007', '2008', '2009', '2010']
+    sessions = ['200405', '200506' ,'200607', '200708', '200809' ,'200910']
+
+    for session in sessions:
+        for year in years:
+            for month in months:
+                for day in days:
+
+                    date = year[2:4]+month+day
+                    if year[2] == '0':
+                        dateShort = year[3:4]+month+day
+                    else:
+                        dateShort = date
+                    # print "Trying session: ",session, " Date: ", date
+                    contentsURL1 = "https://publications.parliament.uk/pa/cm"+session+"/cmhansrd/cm"+date+"/debindx/"+dateShort+"-x.htm"
+                    contentsURL2 = "https://publications.parliament.uk/pa/cm"+session+"/cmhansrd/vo"+date+"/debindx/"+dateShort+"-x.htm"
+
+                    contentsPage1 = urllib2.urlopen(contentsURL1).read()
+                    contentsPage2 = urllib2.urlopen(contentsURL2).read()
+
+                    contentsSoup1 = BeautifulSoup(contentsPage1,'lxml')
+                    contentsSoup2 = BeautifulSoup(contentsPage2,'lxml')
+
+                    if contentsSoup1.find("h1"):
+
+                    response1 = contentsSoup1.find("h1").getText().strip()
+                    response2 = contentsSoup2.find("h1").getText().strip()
+
+                    if response1 != "Page cannot be found":
+                        print "Correct CM URL: ",contentsURL1
+                    elif response2 != "Page cannot be found":
+                        print "Correct VO URL: ",contentsURL2
+
+                    # if contentsSoup.find("h1").getText().strip() != "Page cannot be found":
+                    #     print "Page exists"
+                    #     print "URL is: ", contentsURL
+                    #     # contentsResults = contentsSoup.find_all("p", {"style": "text-align:center;text-transform:capitalize;margin:0 !important;padding:2px;"})
+                    #     # searchURL = contentsResults.find_all('a', href=True)['href']
+                    #     # searchPage = urllib2.urlopen(searchURL).read()
+                    #     # searchSoup = BeautifulSoup(searchPage,'lxml')
+                    #     #
+                    #     # searchResults = searchSoup.body.find_all(string=re.compile('.*{0}.*'.format(searchTerm)), recursive=True)
+                    #     # if len(searchResults) > 0:
+                    #     #     for result in searchResults:
+                    #     #         contribDateFormat = datetime.strptime(" ".join([day, month, year]), '%d %m %Y')
+                    #     #         contributionDates.append(contribDateFormat)
+                    # else:
+                    #     print "Page doesn't exist"
+                    #     print "URL is: ", contentsURL
+                    #     print "..."
+
+    return contributionDates
+
+def getNewContribs(searchTerm):
+
+    contributionDates = []
 
     # search new records from 2006-present day
 
@@ -80,14 +146,16 @@ def plotContribs(searchTerm,contributionDates):
         uniqueCounts.append(contributionDates.count(date))
 
     matplotlib.rcParams['toolbar'] = 'None' # switch off toolbar
-    bars = plt.bar(uniqueDates,uniqueCounts,color='red')
+    plt.bar(uniqueDates,uniqueCounts,color='b',width=5)
+    plt.title(searchTerm)
     plt.gcf().autofmt_xdate()
     plt.show()
 
     return uniqueDates, uniqueCounts
 
-searchTerm = "corruption"
-contributionDates = getOldContribs(searchTerm)
-contributionDates = getNewContribs(searchTerm,contributionDates)
-[uniqueDates, uniqueCounts] = plotContribs(searchTerm,contributionDates)
+searchTerm = "internet"
+# contributionDatesOld = getOldContribs(searchTerm)
+contributionDatesMid = getMidContribs(searchTerm)
+# contributionDatesNew = getNewContribs(searchTerm)
+# [uniqueDates, uniqueCounts] = plotContribs(searchTerm,contributionDatesNew)
 
